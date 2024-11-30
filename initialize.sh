d2fb0809c9625a0d02015d62bb4908613c9c1a8b
CotSezer <cotsezer2000@gmail.com>
2024-11-30 15:24:59 -0500

    disk 50

--- initialize.sh
+++ initialize.sh
@@ -2,11 +2,11 @@
 
 # Variables
 IMAGE_NAME="storage_vgc.img"
-IMAGE_SIZE="1M" # Size of the image file
+IMAGE_SIZE="50M" # Size of the image file
 
 # Create a new disk image
 echo "Creating $IMAGE_NAME..."
-dd if=/dev/zero of=$IMAGE_NAME bs=1M count=1 status=progress
+dd if=/dev/zero of=$IMAGE_NAME bs=1M count=50 status=progress
 echo "Formatting $IMAGE_NAME as ext4..."
 sudo mkfs.ext4 $IMAGE_NAME
 
