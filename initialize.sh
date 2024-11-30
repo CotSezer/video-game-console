eb9780d723589b1ff2fd29af94c110541d188a8d
CotSezer <cotsezer2000@gmail.com>
2024-11-30 15:05:06 -0500

    initiliazed is updated

--- initialize.sh
+++ initialize.sh
@@ -6,9 +6,9 @@ IMAGE_SIZE="1M" # Size of the image file
 
 # Create a new disk image
 echo "Creating $IMAGE_NAME..."
-dd if=/dev/zero of=$IMAGE_NAME bs=1M count=100 status=progress
+dd if=/dev/zero of=$IMAGE_NAME bs=1M count=1 status=progress
 echo "Formatting $IMAGE_NAME as ext4..."
-mkfs.ext4 $IMAGE_NAME
+sudo mkfs.ext4 $IMAGE_NAME
 
 echo "$IMAGE_NAME created and formatted."
 
